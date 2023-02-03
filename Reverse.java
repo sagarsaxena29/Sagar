26f6995df44dd7e626f68bef76c023fd68ad882e
Sagar <116898728+sagarrdec@users.noreply.github.com>
2023-02-04 00:31:14 +0530

    3-feb

---
+++ Reverse.java
@@ -0,0 +1,19 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int arr[] = {8,5,7,10,20,11,4};
+        int n = arr.length;
+        int i = 0;
+        int j = n  -1;
+        while(i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+        for(int k = 0; k < n; k++) {
+            System.out.print(arr[k] + ",");
+        }
+    }
+}
