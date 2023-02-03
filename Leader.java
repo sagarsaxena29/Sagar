128cb975b9b81c71fc6f561518be856341d4247e
Sagar <116898728+sagarrdec@users.noreply.github.com>
2023-02-04 00:27:40 +0530

    3-feb

---
+++ Leader.java
@@ -0,0 +1,16 @@
+public class Leader {
+         public static void main(String[] args) {
+        int arr[] = {1,4,8,9,5,10,6};
+        int max=0 ;
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]>max){
+                System.out.println(+arr[i]);
+            }
+            max=arr[i];
+        }
+          
+    
+        }
+    }
+    
+
